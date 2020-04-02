fd83b61afb0f35c42749ce2987c79242a2c8b16e
George Arbash <georgearbash98@gmail.com>
2020-04-02 23:12:14 +0300

    fixig some widgets alignment for better UI

--- lib/widgets/tansaction_list.dart
+++ lib/widgets/tansaction_list.dart
@@ -10,78 +10,76 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 700,
-      child: transactions.isEmpty
-          ? Column(
-              children: <Widget>[
-                Text('No transactions added yet!'),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                    height: 300,
-                    child: Image.asset(
-                      'assets/images/waiting.png',
-                      fit: BoxFit.cover,
-                    )),
-              ],
-            )
-          : ListView.builder(
-              itemBuilder: (ctx, index) {
-                return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 15,
-                        ),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                          color: Colors.purple,
-                          width: 2,
-                        )),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          '\$${transactions[index].amount.toStringAsFixed(2)}',
-                          style: TextStyle(
+    return transactions.isEmpty
+        ? Column(
+            children: <Widget>[
+              Text('No transactions added yet!'),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                  height: 400,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
+                  )),
+            ],
+          )
+        : ListView.builder(
+            itemBuilder: (ctx, index) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      )),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        '\$${transactions[index].amount.toStringAsFixed(2)}',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                            color: Colors.purple),
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            transactions[index].title,
+                            style: TextStyle(
+                              fontSize: 25,
                               fontWeight: FontWeight.bold,
-                              fontSize: 18,
-                              color: Colors.purple),
-                        ),
-                      ),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              transactions[index].title,
-                              style: TextStyle(
-                                fontSize: 25,
-                                fontWeight: FontWeight.bold,
-                              ),
                             ),
-                            Text(
-                              DateFormat('yyyy-MM-dd')
-                                  .format(transactions[index].date),
-                              style: TextStyle(color: Colors.grey),
-                            ),
-                          ],
-                        ),
+                          ),
+                          Text(
+                            DateFormat('yyyy-MM-dd')
+                                .format(transactions[index].date),
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ],
                       ),
-                      IconButton(
-                        icon: Icon(Icons.delete),
-                        onPressed: () {
-                          deleteTransaction(index);
-                        },
-                      )
-                    ],
-                  ),
-                );
-              },
-              itemCount: transactions.length,
-            ),
-    );
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.delete),
+                      color: Colors.red,
+                      onPressed: () {
+                        deleteTransaction(index);
+                      },
+                    )
+                  ],
+                ),
+              );
+            },
+            itemCount: transactions.length,
+          );
   }
 }
